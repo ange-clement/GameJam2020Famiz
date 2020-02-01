dc2a679774764bc6acdbe04158ad76e4c74ba989
ange-clement <47100137+ange-clement@users.noreply.github.com>
2020-02-01 12:40:04 +0100

    triggerPrefab

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public AudioSource sourceAudio;
+    public AudioClip[] clips;
+
+    private bool collided = false;
+
+    private void Update()
+    {
+        if (collided && !sourceAudio.isPlaying)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !sourceAudio.isPlaying) {
+            collided = true;
+
+            int randomIndex = Random.Range(0, clips.Length);
+            sourceAudio.PlayOneShot(clips[randomIndex]);
+        }
+    }
+}
