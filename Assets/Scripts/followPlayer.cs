c2a6809499bec9bac1b926e851f71d81a888a61d
ange-clement <47100137+ange-clement@users.noreply.github.com>
2020-02-01 19:29:00 +0100

    tuto

--- Assets/Scripts/followPlayer.cs
+++ Assets/Scripts/followPlayer.cs
@@ -6,19 +6,28 @@ public class followPlayer : MonoBehaviour
 {
     public Transform target;
     public float smoothTime = 0.3F;
+    public float leftBoundary;
+    public float rightBoundary;
+
     private Vector3 velocity = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 targetPosition = new Vector3(target.position.x, 1.25f, -10);
-
+        Vector3 targetPosition = Vector3.zero;
+        if (target.position.x > leftBoundary && target.position.x < rightBoundary)
+        {
+            targetPosition = new Vector3(target.position.x, 0, -10);
+        }
+        else if (target.position.x < leftBoundary)
+        {
+            targetPosition = new Vector3(leftBoundary, 0, -10);
+        }
+        else if (target.position.x > rightBoundary)
+        {
+            targetPosition = new Vector3(rightBoundary, 0, -10);
+        }
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
