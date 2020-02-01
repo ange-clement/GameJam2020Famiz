ce1798a0e46d65b3e5ebe09c5ac116967961ddd8
ange-clement <47100137+ange-clement@users.noreply.github.com>
2020-02-01 16:44:04 +0100

    chaise

---
+++ Assets/Scripts/chaiseSoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chaiseSoundManager : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.magnitude > 0.1f)
+        {
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+        }
+        else if (rb.velocity.magnitude < 0.1f)
+        {
+            if (source.isPlaying)
+            {
+                source.Stop();
+            }
+        }
+    }
+}
