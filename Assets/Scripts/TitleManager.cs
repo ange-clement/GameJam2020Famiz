9d37df3ec3f043f8c71d89740529e03515d9bfce
ange-clement <47100137+ange-clement@users.noreply.github.com>
2020-02-01 18:29:17 +0100

    menu musique

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,25 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
     public string playSceneName;
-    // Start is called before the first frame update
-    void Start()
+    public float fadeOutTime = 3f;
+    public TextMeshProUGUI[] texts;
+    public Image[] images;
+
+    public AudioSource VfxSource;
+    public AudioClip playSound;
+
+    private bool isFading = false;
+    private float alpha = 1f;
+    private float alphaSpeed;
+
+    private void Start()
     {
-        
+        alphaSpeed = fadeOutTime / 8f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isFading)
+        {
+            foreach (TextMeshProUGUI text in texts)
+            {
+                text.alpha = alpha;
+            }
+            foreach (Image image in images)
+            {
+                image.color = new Color(image.material.color.r, image.material.color.g, image.material.color.b, alpha);
+            }
+            alpha -= alphaSpeed * Time.deltaTime;
+        }
     }
 
     public void Jouer()
     {
+        isFading = true;
+        VfxSource.PlayOneShot(playSound);
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(fadeOutTime);
         SceneManager.LoadScene(playSceneName);
     }
 }
